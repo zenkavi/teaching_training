4114fb1b23ec33edbc46755d2e9580bcb08dba75
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-08-07 11:56:52 -0700

    working on new nb

--- resources/stats/StatsResourcesConfig.R
+++ resources/stats/StatsResourcesConfig.R
@@ -3,4 +3,4 @@ library(rmarkdown)
 
 theme_set(theme_bw())
 
-render_html_nb <- function(file_name, output_dir){rmarkdown::render(file_name, output_dir, html_notebook(toc = T, toc_float = T, toc_depth = 2, code_folding = 'hide'))}
+# render_html_nb <- function(file_name, output_dir){rmarkdown::render(file_name, output_dir, html_notebook(toc = T, toc_float = T, toc_depth = 2, code_folding = 'hide'))}
