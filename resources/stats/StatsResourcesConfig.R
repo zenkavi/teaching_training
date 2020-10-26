d8765c5d7f8ae10cef535d9558cc5c250c7c2fbf
Zeynep Enkavi <zeynepenkavi@gmail.com>
2020-10-26 12:41:23 -0700

    refactoring and cleanup

--- resources/stats/StatsResourcesConfig.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-library(rmarkdown)
-
-theme_set(theme_bw())
-
-# render_html_nb <- function(file_name, output_dir){rmarkdown::render(file_name, output_dir, html_notebook(toc = T, toc_float = T, toc_depth = 2, code_folding = 'hide'))}
